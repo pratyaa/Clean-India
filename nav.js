57afe3ac4a113f93805292c7f1657b48d9ded134
Pratya chugh <41120205+pratyaa@users.noreply.github.com>
2020-04-25 06:41:15 +0530

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,7 @@
+function openNavbar(){
+            document.getElementById("dropmenu").style.display="block";
+            }
+
+        function closeNavbar(){
+              document.getElementById("dropmenu").style.display="none";
+            }
